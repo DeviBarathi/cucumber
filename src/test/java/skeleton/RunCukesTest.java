01a6fd8fa0aa841344a3db4b735081a127312d4a
elango <elango.murugesan@accenture.com>
2019-09-26 12:04:06 +0530

    adding files

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"json:target/cucmber.json"})
+@CucumberOptions(plugin = "json:target/cucmber.json")
 public class RunCukesTest {
 }
